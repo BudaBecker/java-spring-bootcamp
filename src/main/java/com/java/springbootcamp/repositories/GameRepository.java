c4ac914a4e45e5d2060249ac30dd444ea5dc7c87
BudaBecker <cacocidral@hotmail.com>
2025-05-19 21:12:37 -0300

    Game seed

---
+++ src/main/java/com/java/springbootcamp/repositories/GameRepository.java
@@ -0,0 +1,10 @@
+package com.java.springbootcamp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.java.springbootcamp.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    
+    
+}
