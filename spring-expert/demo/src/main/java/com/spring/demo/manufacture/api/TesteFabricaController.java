600df1ab656431ffb592e015b840ae9782883e4a
BudaBecker <cacocidral@hotmail.com>
2026-02-27 16:08:52 -0300

    Add motor annotations and update MontadoraConfig with new motor beans

--- spring-expert/demo/src/main/java/com/spring/demo/manufacture/api/TesteFabricaController.java
+++ spring-expert/demo/src/main/java/com/spring/demo/manufacture/api/TesteFabricaController.java
@@ -3,6 +3,7 @@ package com.spring.demo.manufacture.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.demo.manufacture.CarroStatus;
@@ -11,9 +12,12 @@ import com.spring.demo.manufacture.HondaHRV;
 import com.spring.demo.manufacture.Motor;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
+    // @Qualifier("motorTurbo")
+    @Turbo
     private Motor motor;
 
     @PostMapping
--- spring-expert/demo/src/main/java/com/spring/demo/manufacture/config/MontadoraConfig.java
+++ spring-expert/demo/src/main/java/com/spring/demo/manufacture/config/MontadoraConfig.java
@@ -2,6 +2,7 @@ package com.spring.demo.manufacture.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.spring.demo.manufacture.Motor;
 import com.spring.demo.manufacture.enums.TipoMotor;
@@ -9,14 +10,37 @@ import com.spring.demo.manufacture.enums.TipoMotor;
 @Configuration
 public class MontadoraConfig {
 
-    @Bean
-    public Motor motor() {
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado() {
         var motor = new Motor();
         motor.setCavalos(120);
         motor.setCilindros(4);
         motor.setLitragem(2.0);
         motor.setModelo("XPTO-0");
         motor.setTipoMotor(TipoMotor.ASPIRADO);
+        return motor;
+    }
+
+    @Bean
+    public Motor motorEletrio() {
+        var motor = new Motor();
+        motor.setCavalos(110);
+        motor.setCilindros(3);
+        motor.setLitragem(1.4);
+        motor.setModelo("TH-10");
+        motor.setTipoMotor(TipoMotor.ELETRICO);
+        return motor;
+    }
+
+    @Bean
+    @Primary
+    public Motor motorTurbo() {
+        var motor = new Motor();
+        motor.setCavalos(180);
+        motor.setCilindros(4);
+        motor.setLitragem(1.5);
+        motor.setModelo("XPTO-0");
+        motor.setTipoMotor(TipoMotor.TURBO);
         return motor;
     }
 }
