d1f6243217d87953d691f398bfc50ae60d899436
BudaBecker <cacocidral@hotmail.com>
2026-02-27 17:32:49 -0300

    pre-inserts para modelo de todos

---
+++ spring-expert/demo/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_todo (descricao, fl_concluido) VALUES ('Aprender Spring Boot', false);
+INSERT INTO tb_todo (descricao, fl_concluido) VALUES ('Finalizar projeto de back-end', false);
+INSERT INTO tb_todo (descricao, fl_concluido) VALUES ('Configurar banco de dados H2', true);
+INSERT INTO tb_todo (descricao, fl_concluido) VALUES ('Revisar código da aplicação', false);
+INSERT INTO tb_todo (descricao, fl_concluido) VALUES ('Fazer deploy na nuvem', false);
