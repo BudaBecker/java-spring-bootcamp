0abd1ccbb3d3b60095a347fda00db9f5e969d625
BudaBecker <cacocidral@hotmail.com>
2026-04-21 21:18:19 -0300

    Add Autor and Livro model classes with JPA annotations for library API

---
+++ spring-expert/libraryapi/src/main/java/com/study/libraryapi/model/Autor.java
@@ -0,0 +1,34 @@
+package com.study.libraryapi.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "autor")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
+
+}
