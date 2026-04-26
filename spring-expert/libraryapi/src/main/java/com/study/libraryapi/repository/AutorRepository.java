9cc5568234f4593a9b595cec189c7cb27eab2903
BudaBecker <cacocidral@hotmail.com>
2026-04-26 17:22:33 -0300

    Add method to find Autor by name in AutorRepository

--- spring-expert/libraryapi/src/main/java/com/study/libraryapi/repository/AutorRepository.java
+++ spring-expert/libraryapi/src/main/java/com/study/libraryapi/repository/AutorRepository.java
@@ -1,5 +1,6 @@
 package com.study.libraryapi.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.study.libraryapi.model.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
-
+    List<Autor> findByNome(String nome);
 }
